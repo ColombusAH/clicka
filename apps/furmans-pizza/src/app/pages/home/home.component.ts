621c9a845df0b8e968ea9d6037e36db668b1f419
ColombusAH <hattabavner@gmail.com>
2021-06-04 19:33:06 +0300

    lazy loaded home page component added

---
+++ apps/furmans-pizza/src/app/pages/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'furrmans-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {}
