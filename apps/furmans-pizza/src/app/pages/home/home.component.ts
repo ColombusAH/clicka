1a90af8d40824f4e5a0bffd6cd435c5ff374f3e4
ColombusAH <hattabavner@gmail.com>
2021-06-05 19:06:45 +0300

    base layout created

--- apps/furmans-pizza/src/app/pages/home/home.component.ts
+++ apps/furmans-pizza/src/app/pages/home/home.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'furrmans-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styles: [':host { width: 100%;} '],
 })
 export class HomeComponent {}
