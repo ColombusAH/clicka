b0f4f88677bd884a12d6b42047185ee239e78d83
ColombusAH <hattabavner@gmail.com>
2021-06-05 18:39:25 +0300

    layout module  & header cmp created

--- apps/furmans-pizza/src/app/app.module.ts
+++ apps/furmans-pizza/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, LayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
